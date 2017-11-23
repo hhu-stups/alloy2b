1abe0c9775c8211d7c9e6cf5aad6b57954f41a7e
Joshua Schmidt <joshua.schmidt@uni-duesseldorf.de>
2017-11-23 13:55:42 +0100

    add some operators

--- src/main/kotlin/de/hhu/stups/alloy2b/ast/Operator.kt
+++ src/main/kotlin/de/hhu/stups/alloy2b/ast/Operator.kt
@@ -1,22 +1,34 @@
 package de.hhu.stups.alloy2b.ast
 
 enum class Operator {
-    LONE, SET, ALL, JOIN, EQUAL, UNION, NO, IN, CLOSURE, CLOSURE1, ONE, SOME;
+    LONE, SET, ALL, JOIN, EQUAL, UNION, INTERSECTION, DIFFERENCE, NO, IN, CLOSURE, CLOSURE1, ONE, SOME, AND, OR,
+    IMPLIES, IFF, GREATER, GREATER_EQUAL, LESS, LESS_EQUAL, CARD, DOM_RESTR, RAN_RESTR, OVERRIDE, ABSTRACT;
 
     companion object {
         fun fromString(op: String): Operator = when (op) {
             "lone" -> LONE
             "set" -> SET
             "all" -> ALL
+            "++" -> OVERRIDE
             "." -> JOIN
+            "<:" -> DOM_RESTR
+            ":>" -> RAN_RESTR
             "=" -> EQUAL
+            ">" -> GREATER
+            ">=" -> GREATER_EQUAL
+            "<" -> LESS
+            "=<" -> LESS_EQUAL
+            "#" -> CARD
+            "&" -> INTERSECTION
             "+" -> UNION
+            "-" -> DIFFERENCE
             "no" -> NO
             "in" -> IN
             "*" -> CLOSURE
             "^" -> CLOSURE1
             "one" -> ONE
             "some" -> SOME
+            "abstract" -> ABSTRACT
             else -> throw UnsupportedOperationException(op)
         }
     }
