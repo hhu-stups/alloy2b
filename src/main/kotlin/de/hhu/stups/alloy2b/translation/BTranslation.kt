4f5b7c64ccd3abbef7a85c796c62b6efb057bcf6
Joshua Schmidt <joshua.schmidt@uni-duesseldorf.de>
2017-11-23 13:55:06 +0100

    extend translation to B

--- src/main/kotlin/de/hhu/stups/alloy2b/translation/BTranslation.kt
+++ src/main/kotlin/de/hhu/stups/alloy2b/translation/BTranslation.kt
@@ -4,16 +4,16 @@ import de.hhu.stups.alloy2b.ast.*
 import de.hhu.stups.alloy2b.ast.Operator.*
 
 class BTranslation(spec: AlloySpecification) {
-    var sets = emptyList<String>()
-    var constants = emptyList<String>()
-    var properties = emptyList<String>()
-    var assertions = emptyList<String>()
+    val sets = mutableListOf<String>()
+    val constants = mutableListOf<String>()
+    val properties = mutableListOf<String>()
+    val assertions = mutableListOf<String>()
 
-    var signatures = emptyList<String>()
-    var extendingSignatures = mutableMapOf<String,List<String>>();
-    var alloyAssertions = mutableMapOf<String,String>();
+    val signatures = mutableListOf<String>()
+    val extendingSignatures = mutableMapOf<String, List<String>>();
+    val alloyAssertions = mutableMapOf<String, String>();
 
-    fun getTranslation() : String {
+    fun getTranslation(): String {
         val builder = StringBuilder()
 
         builder.appendln("MACHINE alloytranslation")
@@ -41,10 +41,10 @@ class BTranslation(spec: AlloySpecification) {
     private fun addSignatureExtensionProperties() {
         // two signatures extending the same base signature are disjuct
         extendingSignatures.values.forEach({ signatures ->
-            for (i1 in 0..signatures.size-1) {
-                for(i2 in i1..signatures.size-1) {
-                    if(!signatures[i1].equals(signatures[i2])) {
-                        properties += "${signatures[i1]} /\\ ${signatures[i2]} = {}"
+            for (i1 in 0..signatures.size - 1) {
+                for (i2 in i1..signatures.size - 1) {
+                    if (!signatures[i1].equals(signatures[i2])) {
+                        properties.add("${signatures[i1]} /\\ ${signatures[i2]} = {}")
                     }
                 }
             }
@@ -52,7 +52,7 @@ class BTranslation(spec: AlloySpecification) {
     }
 
     private fun translate(stmt: Statement) {
-        when(stmt) {
+        when (stmt) {
             is CheckStatement -> translate(stmt)
             is AssertionStatement -> translate(stmt)
             is SignatureDeclaration -> translate(stmt)
@@ -66,90 +66,108 @@ class BTranslation(spec: AlloySpecification) {
     }
 
     private fun translate(fdec: FactDeclaration) {
-        properties += fdec.expressions.map { e-> translateExpression(e) }.joinToString(" & ")
+        properties.add(fdec.expressions.map { e -> translateExpression(e) }.joinToString(" & "))
     }
 
     private fun translate(stmt: CheckStatement) {
-        stmt.expressions.forEach({e ->
-            when(e) {
-                is IdentifierExpression -> assertions += alloyAssertions[e.name].orEmpty()
-                else -> assertions += translateExpression(e)
+        stmt.expressions.forEach({ e ->
+            when (e) {
+                is IdentifierExpression -> assertions.add(alloyAssertions[e.name].orEmpty())
+                else -> assertions.add(translateExpression(e))
             }
         })
     }
 
     private fun translate(sdec: SignatureDeclaration) {
-        signatures += sdec.name
+        signatures.add(sdec.name)
 
-        if(sdec.signatureExtension == null) {
+        if (sdec.signatureExtension == null) {
             // basic signature -> B set
-            sets = sets + sdec.name
+            sets.add(sdec.name)
         } else {
-            constants = constants + sdec.name
-            when(sdec.signatureExtension) {
+            constants.add(sdec.name)
+            when (sdec.signatureExtension) {
                 is NameSignatureExtension -> {
-                    properties = properties + "${sdec.name} <: ${sdec.signatureExtension.name}"
-                    extendingSignatures[sdec.signatureExtension.name] = extendingSignatures.getOrDefault(sdec.signatureExtension.name, emptyList()) + sdec.name
+                    properties.add("${sdec.name} <: ${sdec.signatureExtension.name}")
+                    extendingSignatures[sdec.signatureExtension.name] =
+                            extendingSignatures.getOrDefault(sdec.signatureExtension.name, emptyList()) + sdec.name
                 }
                 else -> throw UnsupportedOperationException(sdec.signatureExtension.javaClass.canonicalName)
             }
         }
 
         // quantifiers handled by cardinality
-        if(NO in sdec.qualifiers) {
-            properties = properties + "card(${sdec.name}) = 0"
+        if (NO in sdec.qualifiers) {
+            properties.add("card(${sdec.name}) = 0")
         }
-        if(LONE in sdec.qualifiers) {
-            properties = properties + "card(${sdec.name}) =< 1"
+        if (LONE in sdec.qualifiers) {
+            properties.add("card(${sdec.name}) =< 1")
         }
-        if(ONE in sdec.qualifiers) {
-            properties = properties + "card(${sdec.name}) = 1"
+        if (ONE in sdec.qualifiers) {
+            properties.add("card(${sdec.name}) = 1")
         }
-        if(SOME in sdec.qualifiers) {
-            properties = properties + "card(${sdec.name}) >= 1"
+        if (SOME in sdec.qualifiers) {
+            properties.add("card(${sdec.name}) >= 1")
         }
     }
 
     private fun translateExpression(e: Expression) =
-        when(e) {
-            is QuantifiedExpression -> translateExpression(e)
-            is IdentifierExpression -> e.name
-            is BinaryOperatorExpression -> translateExpression(e)
-            is UnaryOperatorExpression -> translateExpression(e)
-            else -> throw UnsupportedOperationException(e.javaClass.canonicalName)
-        }
+            when (e) {
+                is QuantifiedExpression -> translateExpression(e)
+                is IdentifierExpression -> e.name
+                is BinaryOperatorExpression -> translateExpression(e)
+                is UnaryOperatorExpression -> translateExpression(e)
+                else -> throw UnsupportedOperationException(e.javaClass.canonicalName)
+            }
 
 
-    private fun translateExpression(qe: QuantifiedExpression) : String =
-        when(qe.operator) {
-            ALL -> "!(${translateDeclsIDList(qe.decls)}).(${translateDeclsExprList(qe.decls)} => ${qe.expressions.map { e->translateExpression(e) }.joinToString(" & ")})"
-            NO -> "not(#(${translateDeclsIDList(qe.decls)}).(${translateDeclsExprList(qe.decls)} => ${qe.expressions.map { e -> translateExpression(e) }.joinToString(" & ")})"
-            ONE -> "card({${translateDeclsIDList(qe.decls)} | ${translateDeclsExprList(qe.decls)} & ${qe.expressions.map { e -> translateExpression(e) }.joinToString(" & ")}}) = 1"
-            LONE -> "card({${translateDeclsIDList(qe.decls)} | ${translateDeclsExprList(qe.decls)} & ${qe.expressions.map { e -> translateExpression(e) }.joinToString(" & ")}}) =< 1"
+    private fun translateExpression(qe: QuantifiedExpression): String =
+            when (qe.operator) {
+                ALL -> "!(${translateDeclsIDList(qe.decls)}).(${translateDeclsExprList(qe.decls)} => ${qe.expressions.map { e -> translateExpression(e) }.joinToString(" & ")})"
+                NO -> "not(#(${translateDeclsIDList(qe.decls)}).(${translateDeclsExprList(qe.decls)} => ${qe.expressions.map { e -> translateExpression(e) }.joinToString(" & ")})"
+                ONE -> "card({${translateDeclsIDList(qe.decls)} | ${translateDeclsExprList(qe.decls)} & ${qe.expressions.map { e -> translateExpression(e) }.joinToString(" & ")}}) = 1"
+                LONE -> "card({${translateDeclsIDList(qe.decls)} | ${translateDeclsExprList(qe.decls)} & ${qe.expressions.map { e -> translateExpression(e) }.joinToString(" & ")}}) =< 1"
+                else -> throw UnsupportedOperationException(qe.operator.name)
+            }
+
+    private fun translateExpression(qe: BinaryOperatorExpression): String {
+        val symbol: String
+        when (qe.operator) {
+            JOIN -> return translateJoin(qe)
+            OVERRIDE -> symbol = "<+"
+            DOM_RESTR -> symbol = "<|"
+            RAN_RESTR -> symbol = "|>"
+            IN -> symbol = ":"
+            EQUAL -> symbol = "="
+            INTERSECTION -> symbol = "/\\"
+            UNION -> symbol = "\\/"
+            DIFFERENCE -> symbol = "-"
+            GREATER -> symbol = ">"
+            GREATER_EQUAL -> symbol = ">="
+            LESS -> symbol = "<"
+            LESS_EQUAL -> symbol = "=<"
+            AND -> symbol = "&"
+            OR -> symbol = "or"
+            IMPLIES -> symbol = "=>"
+            IFF -> symbol = "<=>"
             else -> throw UnsupportedOperationException(qe.operator.name)
         }
+        return "${translateExpression(qe.left)} $symbol ${translateExpression(qe.right)}"
+    }
 
-    private fun translateExpression(qe: BinaryOperatorExpression) : String =
-        when(qe.operator) {
-            JOIN -> translateJoin(qe)
-            IN -> "${translateExpression(qe.left)} : ${translateExpression(qe.right)}"
-            EQUAL -> "${translateExpression(qe.left)} = ${translateExpression(qe.right)}"
-            UNION -> "${translateExpression(qe.left)} \\/ ${translateExpression(qe.right)}"
-            else -> throw UnsupportedOperationException(qe.operator.name)
-        }
+    private fun translateExpression(qe: UnaryOperatorExpression): String =
+            when (qe.operator) {
+                CLOSURE -> "closure(${translateExpression(qe.expression)})"
+                CLOSURE1 -> "closure1(${translateExpression(qe.expression)})"
+                NO -> "${translateExpression(qe.expression)} = {}"
+                CARD -> "card(${translateExpression(qe.expression)})"
+                else -> throw UnsupportedOperationException(qe.operator.name)
+            }
 
-    private fun translateExpression(qe: UnaryOperatorExpression) : String =
-        when(qe.operator) {
-            CLOSURE -> "closure(${translateExpression(qe.expression)})"
-            CLOSURE1 -> "closure1(${translateExpression(qe.expression)})"
-            NO -> "${translateExpression(qe.expression)} = {}"
-            else -> throw UnsupportedOperationException(qe.operator.name)
-        }
-
-    private fun translateJoin(je: BinaryOperatorExpression) : String {
-        if(translateExpression(je.left) in signatures) {
+    private fun translateJoin(je: BinaryOperatorExpression): String {
+        if (translateExpression(je.left) in signatures) {
             return "${translateExpression(je.left)}[${translateExpression(je.right)}]"
-        } else if(translateExpression(je.right) in signatures) {
+        } else if (translateExpression(je.right) in signatures) {
             return "${translateExpression(je.left)}~[${translateExpression(je.right)}]"
         } else {
             return "(${translateExpression(je.left)} ; ${translateExpression(je.right)})"
@@ -157,9 +175,9 @@ class BTranslation(spec: AlloySpecification) {
     }
 
     private fun translateDeclsIDList(decls: List<Decl>) =
-        decls.map { d -> d.name }.joinToString(", ")
+            decls.map { d -> d.name }.joinToString(", ")
 
     private fun translateDeclsExprList(decls: List<Decl>) =
-        decls.map { d -> "${d.name} : ${translateExpression(d.expression)}" }.joinToString(", ")
+            decls.map { d -> "${d.name} : ${translateExpression(d.expression)}" }.joinToString(", ")
 }
 
