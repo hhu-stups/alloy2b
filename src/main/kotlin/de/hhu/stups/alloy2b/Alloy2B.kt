58d27811a7c84d15601cf5dbd9de7bde94af0ead
Sebastian Krings <sebastian@krin.gs>
2017-11-24 11:09:47 +0100

    update main to allow print to file

--- src/main/kotlin/de/hhu/stups/alloy2b/Alloy2B.kt
+++ src/main/kotlin/de/hhu/stups/alloy2b/Alloy2B.kt
@@ -6,9 +6,7 @@ import de.hhu.stups.alloy2b.translation.BTranslation
 import java.io.File
 
 fun main(args: Array<String>) {
-    if(args.size != 1) {
-        println("use filename as single command line argument")
-    } else {
+    if(args.size == 1) {
         println("Parsing ...")
         val parseResult = parse(File(args[0]))
 
@@ -16,5 +14,17 @@ fun main(args: Array<String>) {
             println("Translating ...")
             println(BTranslation((parseResult.root.toAst(false))).getTranslation())
         }
+    } else if (args.size == 2) {
+        val parseResult = parse(File(args[0]))
+        if (parseResult.root != null) {
+            File(args[1]).printWriter().use { out ->
+                out.println(BTranslation((parseResult.root.toAst(false))).getTranslation()) }
+        }
+        else {
+            println("Parsing or Translation failed")
+        }
+    } else {
+        println("use alloy filename as single command line argument (translation will be printed to stdout)")
+        println("or alloy filename and .mch file as two command line arguments")
     }
 }
