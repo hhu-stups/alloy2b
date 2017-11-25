fa80d1476a1e51eb90163a961a7744785a73884b
Joshua Schmidt <joshua.schmidt@uni-duesseldorf.de>
2017-11-25 12:57:22 +0100

    add test case

--- src/test/kotlin/de/hhu/stups/alloy2b/TranslationTest.kt
+++ src/test/kotlin/de/hhu/stups/alloy2b/TranslationTest.kt
@@ -6,9 +6,16 @@ import org.junit.Assert
 import org.junit.Test as test
 
 class TranslationTest {
+
     @test
     fun translateFileSystemModel() {
         val expected = getResourceAsString("FileSystem.mch");
         Assert.assertEquals(expected, BTranslation(parseResource("FileSystem.als").toAst(false)).getTranslation())
+    }
+
+    @test
+    fun translateFileSystemModel2() {
+        val expected = getResourceAsString("FileSystem2.mch");
+        Assert.assertEquals(expected, BTranslation(parseResource("FileSystem2.als").toAst(false)).getTranslation())
     }
 }
