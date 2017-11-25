3d03b1b29bedfaa6afbadad72710be0bf524626d
Joshua Schmidt <joshua.schmidt@uni-duesseldorf.de>
2017-11-25 12:49:47 +0100

    reduce nesting

--- src/main/kotlin/de/hhu/stups/alloy2b/Alloy2B.kt
+++ src/main/kotlin/de/hhu/stups/alloy2b/Alloy2B.kt
@@ -6,25 +6,38 @@ import de.hhu.stups.alloy2b.translation.BTranslation
 import java.io.File
 
 fun main(args: Array<String>) {
-    if(args.size == 1) {
-        println("Parsing ...")
-        val parseResult = parse(File(args[0]))
-
-        if (parseResult.root != null) {
-            println("Translating ...")
-            println(BTranslation((parseResult.root.toAst(false))).getTranslation())
-        }
-    } else if (args.size == 2) {
-        val parseResult = parse(File(args[0]))
-        if (parseResult.root != null) {
-            File(args[1]).printWriter().use { out ->
-                out.println(BTranslation((parseResult.root.toAst(false))).getTranslation()) }
-        }
-        else {
-            println("Parsing or Translation failed")
-        }
-    } else {
-        println("use alloy filename as single command line argument (translation will be printed to stdout)")
-        println("or alloy filename and .mch file as two command line arguments")
+    if (args.size == 1) {
+        translateToConsole(args[0])
+        return
     }
+    if (args.size == 2) {
+        translateToFile(args[0], args[1])
+        return
+    }
+    println("use alloy filename as single command line argument (translation will be printed to stdout)")
+    println("or alloy filename and .mch file as two command line arguments")
 }
+
+fun translateToConsole(inputFilePath: String) {
+    println("Parsing ...")
+    val parseResult = parse(File(inputFilePath))
+    if (parseResult.root != null) {
+        println("Translating ...")
+        println(BTranslation((parseResult.root.toAst(false))).getTranslation())
+    }
+    println("Parsing failed")
+}
+
+fun translateToFile(inputFilePath: String, outputFilePath: String) {
+    println("Parsing ...")
+    val parseResult = parse(File(inputFilePath))
+    if (parseResult.root != null) {
+        println("Translating ...")
+        File(outputFilePath).printWriter().use { out ->
+            out.println(BTranslation((parseResult.root.toAst(false))).getTranslation())
+        }
+        return
+    }
+    println("Parsing failed")
+}
+
