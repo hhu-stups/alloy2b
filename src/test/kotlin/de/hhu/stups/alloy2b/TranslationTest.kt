489d870cb02c1a4b02e33e238cc46a448c9283e3
Sebastian Krings <sebastian@krin.gs>
2017-11-29 16:27:22 +0100

    simplify

--- src/test/kotlin/de/hhu/stups/alloy2b/TranslationTest.kt
+++ src/test/kotlin/de/hhu/stups/alloy2b/TranslationTest.kt
@@ -2,7 +2,7 @@ package de.hhu.stups.alloy2b
 
 import de.hhu.stups.alloy2b.ast.toAst
 import de.hhu.stups.alloy2b.translation.BTranslation
-import org.junit.Assert
+import org.junit.Assert.*
 import org.junit.Ignore
 import org.junit.Test as test
 
@@ -11,31 +11,31 @@ class TranslationTest {
     @test
     fun translateFileSystem() {
         val expected = getResourceAsString("FileSystem.mch")
-        Assert.assertEquals(expected, BTranslation(parseResource("FileSystem.als").toAst(false)).getTranslation())
+        assertEquals(expected, BTranslation(parseResource("FileSystem.als").toAst(false)).getTranslation())
     }
 
     @test
     fun translateFileSystem2() {
         val expected = getResourceAsString("FileSystem2.mch")
-        Assert.assertEquals(expected, BTranslation(parseResource("FileSystem2.als").toAst(false)).getTranslation())
+        assertEquals(expected, BTranslation(parseResource("FileSystem2.als").toAst(false)).getTranslation())
     }
 
     @test
     fun translateFileSystem3() {
         val expected = getResourceAsString("FileSystem3.mch")
-        Assert.assertEquals(expected, BTranslation(parseResource("FileSystem3.als").toAst(false)).getTranslation())
+        assertEquals(expected, BTranslation(parseResource("FileSystem3.als").toAst(false)).getTranslation())
     }
 
     @test
     fun translateExtendedFileSystem() {
         val expected = getResourceAsString("ExtendedFileSystem.mch")
-        Assert.assertEquals(expected, BTranslation(parseResource("ExtendedFileSystem.als").toAst(false)).getTranslation())
+        assertEquals(expected, BTranslation(parseResource("ExtendedFileSystem.als").toAst(false)).getTranslation())
     }
 
     @test
     fun translateSelfGrandpas() {
         val expected = getResourceAsString("SelfGrandpas.mch")
-        Assert.assertEquals(expected, BTranslation(parseResource("SelfGrandpas.als").toAst(false)).getTranslation())
+        assertEquals(expected, BTranslation(parseResource("SelfGrandpas.als").toAst(false)).getTranslation())
     }
 
     @test
@@ -48,13 +48,13 @@ class TranslationTest {
     @test
     fun translateQueens() {
         val expected = getResourceAsString("queens.mch")
-        Assert.assertEquals(expected, BTranslation(parseResource("queens.als").toAst(false)).getTranslation())
+        assertEquals(expected, BTranslation(parseResource("queens.als").toAst(false)).getTranslation())
     }
 
     @test
     fun translateSudoku() {
         val expected = getResourceAsString("sudoku1.mch")
-        Assert.assertEquals(expected, BTranslation(parseResource("sudoku1.als").toAst(false)).getTranslation())
+        assertEquals(expected, BTranslation(parseResource("sudoku1.als").toAst(false)).getTranslation())
     }
 
     @test @Ignore
