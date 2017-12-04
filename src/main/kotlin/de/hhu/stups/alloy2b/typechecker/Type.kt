e21169f3e897faaa93da0d70059c6bc6b3da5fb2
Joshua Schmidt <joshua.schmidt@uni-duesseldorf.de>
2017-12-04 11:48:45 +0100

    extend types to store explicit signature types

--- src/main/kotlin/de/hhu/stups/alloy2b/typechecker/Type.kt
+++ src/main/kotlin/de/hhu/stups/alloy2b/typechecker/Type.kt
@@ -1,22 +1,20 @@
 package de.hhu.stups.alloy2b.typechecker
 
-enum class Types {
-    SCALAR, SET, RELATION, UNTYPED, INTEGER
-}
+interface ExplicitType
 
-class Type(type:Types = Types.UNTYPED) {
+class Type(type: ExplicitType = Untyped()) {
     var currentType = type
 
-    fun setType(type: Types) {
-        if(currentType == Types.UNTYPED) {
+    fun setType(type: ExplicitType) {
+        if (currentType is Untyped) {
             currentType = type
-        } else if(currentType != type) {
+        } else if (currentType != type) {
             throw UnsupportedOperationException("Type Checking failed")
         }
     }
 
     fun untyped(): Boolean {
-        return currentType == Types.UNTYPED
+        return currentType is Untyped
     }
 
     override fun toString(): String {
@@ -24,10 +22,26 @@ class Type(type:Types = Types.UNTYPED) {
     }
 
     override fun equals(other: Any?): Boolean {
-        if(other is Type) {
+        if (other is Type) {
             return currentType.equals(other.currentType)
         } else {
             return false
         }
     }
+
+    override fun hashCode(): Int {
+        return currentType.hashCode()
+    }
 }
+
+data class Signature(val subType: String) : ExplicitType
+
+data class Set(val subType: ExplicitType) : ExplicitType
+
+data class Relation(val leftType: ExplicitType, val rightType: ExplicitType) : ExplicitType
+
+data class Scalar(val subType: ExplicitType) : ExplicitType
+
+class Integer : ExplicitType
+
+class Untyped : ExplicitType
