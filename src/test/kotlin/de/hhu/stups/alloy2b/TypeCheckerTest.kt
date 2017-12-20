8c6c6bf2bb3a7779d193953aa459b21d774a9f01
Sebastian Krings <sebastian@krin.gs>
2017-12-20 10:18:12 +0100

    add type checker tests

---
+++ src/test/kotlin/de/hhu/stups/alloy2b/TypeCheckerTest.kt
@@ -0,0 +1,37 @@
+package de.hhu.stups.alloy2b
+
+import de.hhu.stups.alloy2b.ast.SignatureDeclarations
+import de.hhu.stups.alloy2b.ast.toAst
+import de.hhu.stups.alloy2b.parsing.ParserFacade
+import de.hhu.stups.alloy2b.typechecker.Signature
+import de.hhu.stups.alloy2b.typechecker.Type
+import de.hhu.stups.alloy2b.typechecker.TypeChecker
+import junit.framework.TestCase.assertEquals
+import de.hhu.stups.alloy2b.typechecker.Set
+import org.junit.Assert
+import org.junit.Test
+
+class TypeCheckerTest {
+    @Test
+    fun typeSubSignatures() {
+        val code = "sig test {}\nsig test2 {}"
+
+        val result = ParserFacade.parse(code)
+        val ast = result.root!!.toAst()
+        val errors = result.errors
+
+        Assert.assertTrue(errors.isEmpty())
+
+        TypeChecker(ast)
+
+        var sigdecls = ast.declarations.get(0) as SignatureDeclarations
+        assertEquals(1,sigdecls.signatures.size)
+        assertEquals(Type(Set(Type(Signature("test")))),sigdecls.signatures.first().name.type)
+
+
+        sigdecls = ast.declarations.get(1) as SignatureDeclarations
+        assertEquals(1,sigdecls.signatures.size)
+        assertEquals(Type(Set(Type(Signature("test")))),sigdecls.signatures.first().name.type)
+
+    }
+}
