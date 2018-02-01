649261c6c4b3ab665266ae94defd7b841f6ea3b2
Joshua Schmidt <joshua.schmidt@uni-duesseldorf.de>
2018-02-01 22:59:57 +0100

    add prefix operators from util/integer to Operator enum

--- src/main/kotlin/de/hhu/stups/alloy2b/ast/Operator.kt
+++ src/main/kotlin/de/hhu/stups/alloy2b/ast/Operator.kt
@@ -2,8 +2,9 @@ package de.hhu.stups.alloy2b.ast
 
 enum class Operator {
     LONE, SET, ALL, JOIN, EQUAL, PLUS, INTERSECTION, MINUS, NO, IN, CLOSURE, CLOSURE1, ONE, SOME, AND, OR,
-    IMPLICATION, IFF, GREATER, GREATER_EQUAL, LESS, LESS_EQUAL, CARD, DOM_RESTR, RAN_RESTR, OVERRIDE, ABSTRACT, CARTESIAN,
-    TOTAL_FUNCTION, PARTIAL_FUNCTION, BIJECTIVE_FUNCTION, INVERSE, NOT, SEQ;
+    IMPLICATION, IFF, GREATER, GREATER_EQUAL, LESS, LESS_EQUAL, CARD, DOM_RESTR, RAN_RESTR, OVERRIDE, ABSTRACT,
+    CARTESIAN, TOTAL_FUNCTION, PARTIAL_FUNCTION, BIJECTIVE_FUNCTION, INVERSE, NOT, SEQ,
+    INT_PLUS, INT_MINUS, INT_DIV, INT_MODULO, INT_PRODUCT, INT_SUM, INT_MAX, INT_MIN;
 
     companion object {
         fun fromString(op: String): Operator =
@@ -48,6 +49,14 @@ enum class Operator {
                     "!" -> NOT
                     "not" -> NOT
                     "seq" -> SEQ
+                    "plus" -> INT_PLUS
+                    "minus" -> INT_MINUS
+                    "div" -> INT_DIV
+                    "rem" -> INT_MODULO
+                    "mul" -> INT_PRODUCT
+                    "sum" -> INT_SUM
+                    "max" -> INT_MAX
+                    "min" -> INT_MIN
                     else -> throw UnsupportedOperationException(op)
                 }
     }
