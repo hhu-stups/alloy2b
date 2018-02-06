33e1d5f8f920b39d99829274e158399950957842
Joshua Schmidt <joshua.schmidt@uni-duesseldorf.de>
2018-02-06 22:56:35 +0100

    extend translation

--- src/main/kotlin/de/hhu/stups/alloy2b/translation/ExpressionTranslator.kt
+++ src/main/kotlin/de/hhu/stups/alloy2b/translation/ExpressionTranslator.kt
@@ -2,26 +2,31 @@ package de.hhu.stups.alloy2b.translation
 
 import edu.mit.csail.sdg.alloy4compiler.ast.*
 
-class ExpressionTranslator: VisitReturn<String>() {
+class ExpressionTranslator : VisitReturn<String>() {
     override fun visit(p0: ExprBinary): String =
-            when(p0.op) {
+            when (p0.op) {
                 ExprBinary.Op.EQUALS -> "${p0.left.accept(this)} = ${p0.right.accept(this)}"
-                ExprBinary.Op.JOIN -> translateJoin(p0.left,p0.right)
-                else -> TODO("Binary operator not implemented: " + p0.op)
+                ExprBinary.Op.JOIN -> translateJoin(p0.left, p0.right)
+                else -> throw UnsupportedOperationException("Binary operator not implemented: " + p0.op)
             }
 
     override fun visit(p0: ExprList): String =
-            when(p0.op) {
-                ExprList.Op.AND -> p0.args.map { it.accept(this) }.joinToString(" & ")
-                else -> TODO("List operator not implemented: " + p0.op)
+            when (p0.op) {
+                ExprList.Op.AND -> p0.args.joinToString(" & ") { it.accept(this) }
+                else -> throw UnsupportedOperationException("List operator not implemented: " + p0.op)
             }
 
     override fun visit(p0: ExprCall?): String {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
-    override fun visit(p0: ExprConstant?): String =
-            p0.toString()
+    override fun visit(p0: ExprConstant?): String {
+        if (p0?.toString()?.toIntOrNull() != null) {
+            // do not sanitize integers
+            return p0.toString()
+        }
+        return AlloyAstTranslation.sanitizeIdentifier(p0.toString())
+    }
 
     override fun visit(p0: ExprITE?): String {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
@@ -36,35 +41,35 @@ class ExpressionTranslator: VisitReturn<String>() {
     }
 
     override fun visit(p0: ExprUnary): String =
-            when(p0.op) {
+            when (p0.op) {
                 ExprUnary.Op.NOOP -> p0.sub.accept(this)
                 ExprUnary.Op.CARDINALITY -> "card(${p0.sub.accept(this)})"
                 ExprUnary.Op.ONEOF -> p0.sub.accept(this) // TODO do something here
-                else -> TODO("Unary operator not implemented: " + p0.op)
+                ExprUnary.Op.SETOF -> "<: ${p0.sub.accept(this)}"
+                else -> throw UnsupportedOperationException("Unary operator not implemented: " + p0.op)
             }
 
     override fun visit(p0: ExprVar): String =
-            p0.toString()
+            AlloyAstTranslation.sanitizeIdentifier(p0.toString())
 
     override fun visit(p0: Sig): String =
-            p0.label
+            AlloyAstTranslation.sanitizeIdentifier(p0.label)
 
     override fun visit(p0: Sig.Field): String =
-            p0.label
+            AlloyAstTranslation.sanitizeIdentifier(p0.label)
 
     private fun translateJoin(left: Expr, right: Expr): String {
         val tLeft = left.accept(this)
         val tRight = right.accept(this)
-        if(left.type().arity() == 1 && right.type().arity() == 2) {
+        if (left.type().arity() == 1 && right.type().arity() == 2) {
             return "$tRight[$tLeft]"
         }
-        if(left.type().arity() == 2 && right.type().arity() == 1) {
+        if (left.type().arity() == 2 && right.type().arity() == 1) {
             return "$tLeft~[$tRight]"
         }
-        if(left.type().arity() == 2 && right.type().arity() == 2) {
+        if (left.type().arity() == 2 && right.type().arity() == 2) {
             return "($tLeft ; $tRight)"
         }
-        TODO("Join Translation failed")
+        throw UnsupportedOperationException("Join translation failed.")
     }
-
 }
