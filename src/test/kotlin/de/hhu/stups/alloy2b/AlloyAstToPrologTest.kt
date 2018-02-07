c44541d70d2944c0f3453c355708a54adb53db5a
Joshua Schmidt <joshua.schmidt@uni-duesseldorf.de>
2018-02-07 23:57:13 +0100

    add test file with no tests by now but an example of the usage of AlloyAstToProlog

---
+++ src/test/kotlin/de/hhu/stups/alloy2b/AlloyAstToPrologTest.kt
@@ -0,0 +1,13 @@
+package de.hhu.stups.alloy2b
+
+import de.hhu.stups.alloy2b.translation.prolog.AlloyAstToProlog
+import org.junit.Ignore
+import org.junit.Test
+
+class AlloyAstToPrologTests {
+    @Test
+    @Ignore
+    fun translateFileSystem() {
+        println(AlloyAstToProlog("/FileSystem.als").getPrologTerm())
+    }
+}
