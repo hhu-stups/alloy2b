5b2f0d3e5e09f87e0c95c62e28b09e08cd2b15e0
Joshua Schmidt <joshua.schmidt@uni-duesseldorf.de>
2018-02-08 16:46:46 +0100

    optionally translate an Alloy model to a Prolog term using the flag -toProlog as the last argument

--- src/main/kotlin/de/hhu/stups/alloy2b/Alloy2B.kt
+++ src/main/kotlin/de/hhu/stups/alloy2b/Alloy2B.kt
@@ -3,22 +3,32 @@ package de.hhu.stups.alloy2b
 import de.hhu.stups.alloy2b.ast.toAst
 import de.hhu.stups.alloy2b.parsing.ParserFacade.parse
 import de.hhu.stups.alloy2b.translation.BTranslation
+import de.hhu.stups.alloy2b.translation.prolog.AlloyAstToProlog
 import java.io.File
 
 fun main(args: Array<String>) {
+    if (args.size == 2 && args[1] == "-toProlog") {
+        translatePrologToConsole(args[0])
+        return
+    }
     if (args.size == 1) {
-        translateToConsole(args[0])
+        translateBToConsole(args[0])
         return
     }
     if (args.size == 2) {
-        translateToFile(args[0], args[1])
+        translateBToFile(args[0], args[1])
         return
     }
     println("Use the alloy file path as a single command line argument to print the translation to stdout.")
     println("Alternatively, use the alloy file path and .mch file path as two command line arguments.")
+    println("Use the flag -toProlog as the last argument to translate an Alloy model to a Prolog term.")
 }
 
-fun translateToConsole(inputFilePath: String) {
+fun translatePrologToConsole(inputFilePath: String) {
+    println(AlloyAstToProlog(inputFilePath).getPrologTerm())
+}
+
+fun translateBToConsole(inputFilePath: String) {
     println("Parsing ...")
     val parseResult = parse(File(inputFilePath))
     if (parseResult.root != null && parseResult.errors.isEmpty()) {
@@ -29,7 +39,7 @@ fun translateToConsole(inputFilePath: String) {
     println("Parsing failed.")
 }
 
-fun translateToFile(inputFilePath: String, outputFilePath: String) {
+fun translateBToFile(inputFilePath: String, outputFilePath: String) {
     println("Parsing ...")
     val parseResult = parse(File(inputFilePath))
     if (parseResult.root != null && parseResult.errors.isEmpty()) {
