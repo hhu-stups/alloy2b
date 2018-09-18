0b85ed92072202526afd5ac885dd9c90d99bad9a
David Schneider <david.schneider@bivab.de>
2018-09-18 16:33:56 +0200

    Added script to regenerate prolog test files

---
+++ regenerate.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+./gradlew jar
+
+version=`./gradlew -q alloy2BVersion`
+
+for a in $(ls src/test/resources/*.als)
+do
+  i=`echo "$a" | awk '{print tolower($0)}'`
+	java -jar "build/libs/alloy2b-${version}.jar" "$i" -toProlog "${i%.als}.pl";
+done
