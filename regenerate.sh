5cddecb4d2b06aca66c561d96d90c7981ac4c544
Joshua Schmidt <joshua.schmidt@uni-duesseldorf.de>
2018-09-27 14:06:51 +0200

    use lower case for output file names

--- regenerate.sh
+++ regenerate.sh
@@ -6,5 +6,5 @@ version=`./gradlew -q alloy2BVersion`
 for a in $(ls src/test/resources/*.als)
 do
   i=`echo "$a" | awk '{print tolower($0)}'`
-	java -jar "build/libs/alloy2b-${version}.jar" "$i" -toProlog "${i%.als}.pl";
+	java -jar "build/libs/alloy2b-${version}.jar" "$a" -toProlog "${i%.als}.pl";
 done
