eb27cbba5f740c0865051b683514a0febd6837b4
Joshua Schmidt <joshua.schmidt@uni-duesseldorf.de>
2018-11-08 21:28:00 +0100

    adapt test file to class renaming

--- src/test/kotlin/de/hhu/stups/alloy2b/Alloy2BParserTests.kt
+++ src/test/kotlin/de/hhu/stups/alloy2b/Alloy2BParserTests.kt
@@ -1,10 +1,10 @@
 package de.hhu.stups.alloy2b
 
-import de.hhu.stups.alloy2b.translation.AlloyAstToProlog
+import de.hhu.stups.alloy2b.translation.Alloy2BParser
 import org.junit.Assert
 import org.junit.Test
 
-class AlloyAstToPrologTests {
+class Alloy2BParserTests {
 
     fun getResourceAsString(resourceName: String): String {
         val expected = object {}.javaClass.getResourceAsStream("/$resourceName")
@@ -14,96 +14,96 @@ class AlloyAstToPrologTests {
     @Test
     fun translateFileSystem() {
         val expected = getResourceAsString("filesystem.pl")
-        Assert.assertEquals(expected, AlloyAstToProlog("/FileSystem.als").getPrologTerm())
+        Assert.assertEquals(expected, Alloy2BParser("/FileSystem.als").getPrologTerm())
     }
 
     @Test
     fun translateFileSystem2() {
         val expected = getResourceAsString("filesystem2.pl")
-        Assert.assertEquals(expected, AlloyAstToProlog("/FileSystem2.als").getPrologTerm())
+        Assert.assertEquals(expected, Alloy2BParser("/FileSystem2.als").getPrologTerm())
     }
 
     @Test
     fun translateCards() {
         val expected = getResourceAsString("cards.pl")
-        Assert.assertEquals(expected, AlloyAstToProlog("/cards.als").getPrologTerm())
+        Assert.assertEquals(expected, Alloy2BParser("/cards.als").getPrologTerm())
     }
 
     @Test
     fun translateSelfGrandpas() {
         val expected = getResourceAsString("selfgrandpas.pl")
-        Assert.assertEquals(expected, AlloyAstToProlog("/SelfGrandpas.als").getPrologTerm())
+        Assert.assertEquals(expected, Alloy2BParser("/SelfGrandpas.als").getPrologTerm())
     }
 
     @Test
     fun translateCrewAlloc() {
         val expected = getResourceAsString("crewalloc.pl")
-        Assert.assertEquals(expected, AlloyAstToProlog("/crewalloc.als").getPrologTerm())
+        Assert.assertEquals(expected, Alloy2BParser("/crewalloc.als").getPrologTerm())
     }
 
     @Test
     fun translateQueens2() {
         val expected = getResourceAsString("queens2.pl")
-        Assert.assertEquals(expected, AlloyAstToProlog("/queens2.als").getPrologTerm())
+        Assert.assertEquals(expected, Alloy2BParser("/queens2.als").getPrologTerm())
     }
 
     @Test
     fun translateRiverCrossing() {
         val expected = getResourceAsString("rivercrossingpuzzle.pl")
-        Assert.assertEquals(expected, AlloyAstToProlog("/RiverCrossingPuzzle.als").getPrologTerm())
+        Assert.assertEquals(expected, Alloy2BParser("/RiverCrossingPuzzle.als").getPrologTerm())
     }
 
     @Test
     fun translateHanoi() {
         val expected = getResourceAsString("hanoi.pl")
-        Assert.assertEquals(expected, AlloyAstToProlog("/hanoi.als").getPrologTerm())
+        Assert.assertEquals(expected, Alloy2BParser("/hanoi.als").getPrologTerm())
     }
 
     @Test
     fun translateEnumTest() {
         val expected = getResourceAsString("enumtest.pl")
-        Assert.assertEquals(expected, AlloyAstToProlog("/EnumTest.als").getPrologTerm())
+        Assert.assertEquals(expected, Alloy2BParser("/EnumTest.als").getPrologTerm())
     }
 
     @Test
     fun translateGraphiso() {
         val expected = getResourceAsString("graphiso.pl")
-        Assert.assertEquals(expected, AlloyAstToProlog("/graphiso.als").getPrologTerm())
+        Assert.assertEquals(expected, Alloy2BParser("/graphiso.als").getPrologTerm())
     }
 
     @Test
     fun translateDisjFieldTest() {
         val expected = getResourceAsString("disjfieldtest.pl")
-        Assert.assertEquals(expected, AlloyAstToProlog("/DisjFieldTest.als").getPrologTerm())
+        Assert.assertEquals(expected, Alloy2BParser("/DisjFieldTest.als").getPrologTerm())
     }
 
     @Test
     fun translateFamilyV1() {
         val expected = getResourceAsString("family-v1.pl")
-        Assert.assertEquals(expected, AlloyAstToProlog("/family-v1.als").getPrologTerm())
+        Assert.assertEquals(expected, Alloy2BParser("/family-v1.als").getPrologTerm())
     }
 
     @Test
     fun translateRelations() {
         val expected = getResourceAsString("relations.pl")
-        Assert.assertEquals(expected, AlloyAstToProlog("/relations.als").getPrologTerm())
+        Assert.assertEquals(expected, Alloy2BParser("/relations.als").getPrologTerm())
     }
 
     @Test
     fun translateViews() {
         val expected = getResourceAsString("views.pl")
-        Assert.assertEquals(expected, AlloyAstToProlog("/views.als").getPrologTerm())
+        Assert.assertEquals(expected, Alloy2BParser("/views.als").getPrologTerm())
     }
 
     @Test
     fun translateSequenceTests() {
         val expected = getResourceAsString("sequence_test.pl")
-        Assert.assertEquals(expected, AlloyAstToProlog("/sequence_test.als").getPrologTerm())
+        Assert.assertEquals(expected, Alloy2BParser("/sequence_test.als").getPrologTerm())
     }
 
     @Test
     fun translateMutex() {
         val expected = getResourceAsString("mutex.pl")
-        Assert.assertEquals(expected, AlloyAstToProlog("/mutex.als").getPrologTerm())
+        Assert.assertEquals(expected, Alloy2BParser("/mutex.als").getPrologTerm())
     }
 }
