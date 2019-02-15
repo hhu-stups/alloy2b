72e50477e26b1cdc12be2606d92e3063a5df7652
Joshua Schmidt <joshua.schmidt@uni-duesseldorf.de>
2019-02-15 16:31:21 +0100

    just clean types that cannot be generalized like sequences

--- src/main/kotlin/de/hhu/stups/alloy2b/translation/ExpressionToProlog.kt
+++ src/main/kotlin/de/hhu/stups/alloy2b/translation/ExpressionToProlog.kt
@@ -126,18 +126,14 @@ class ExpressionToProlog(private val signatures: MutableList<Sig>,
         return generalizeTypes(type)
     }
 
-    @Throws(NotImplementedError::class)
     private fun generalizeType(type: Type): String {
         val typeString = type.toString()
-        if (typeString == "{Int}" || typeString == "{univ}") {
-            return cleanUpType(type)
-        }
         val parents = signatures.filter { type.isSubtypeOf(it.type()) }
         if (type.toString().contains("/Ord")) {
             return "'Ordering'"
         }
-        if (parents.isEmpty()) {
-            throw NotImplementedError("This type is not implemented.")
+        if (parents.isEmpty() || typeString == "{Int}" || typeString == "{univ}") {
+            return cleanUpType(type)
         }
         return cleanUpType(getMostGeneralType(parents))
     }
